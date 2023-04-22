6ef7f7ae0e8206c6406a817b23d40b7fa91b74b9
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2023-04-22 14:45:06 +0530

    Update Arrays.java

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -8,7 +8,9 @@ public class Arrays {
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
-        
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]))
+        }
     }
     
 }
